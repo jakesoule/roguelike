17242a952f35b87b3edf7bc0e8ac8fc5206c7d09
Jake Soule <132782116+jakesoule@users.noreply.github.com>
2023-07-16 23:46:11 -0400

    add engine.py, entity.py, game_map.py, tile_types.py
    Added 4 new classes to further simplify and make more readable, and to add groundwork to building our dungeons

--- actions.py
+++ actions.py
@@ -1,8 +1,19 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
+
 class Action:
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise NotImplementedError()
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
 
 class MovementAction(Action):
     def __init__(self, dx:int, dy:int):
@@ -10,3 +21,15 @@ class MovementAction(Action):
 
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return
+        
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return
+        
+        entity.move(self.dx, self.dy)
