6c8958ea8e251b6b948ecd03f09ae931d9bc7a94
Jake Soule <132782116+jakesoule@users.noreply.github.com>
2023-07-17 19:35:49 -0400

    add fov mechanic

--- game_map.py
+++ game_map.py
@@ -9,9 +9,16 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
+        self.visible = np.full((width, height), fill_value = False, order="F")
+        self.explored = np.full((width, height), fill_value=False, order="F")
+
     def in_bounds(self, x: int, y: int) -> bool:
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default = tile_types.SHROUD
+        )
     
--- tile_types.py
+++ tile_types.py
@@ -15,6 +15,7 @@ tile_dt = np.dtype(
         ("walkable", bool),
         ("transparent", bool),
         ("dark", graphic_dt),
+        ("light", graphic_dt),
     ]
 )
 
@@ -22,14 +23,23 @@ def new_tile(
     walkable: int,
     transparent: int,
     dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
+
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
 floor = new_tile(
-    walkable = True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    walkable = True,
+    transparent=True,
+    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    light=(ord(" "), (255, 255, 255), (200, 180, 50)),
 )
 
 wall = new_tile(
-    walkable = False, transparent = False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    walkable = False,
+    transparent = False,
+    dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    light=(ord(" "), (255, 255, 255), (130, 110, 50))
 )
 
